ee49d3bf5ff27bcade59a774795456b3c076be74
ohaeori <0typhoon416@gmail.com>
2021-03-04 17:08:41 +0900

    Create ohaeori.py

---
+++ programmers/12905/python/ohaeori.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+def solution(clothes):
+    answer = 1
+    dic = defaultdict(int)
+    for i,j in clothes:
+        dic[j] += 1
+    for i in dic:
+        answer *= dic[i]+1
+    
+    return answer -1
