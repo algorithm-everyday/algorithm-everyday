ee4cd289f8fcfe4732e683287882a3350a0c4e83
LeeHanSeong7 <54903040+LeeHanSeong7@users.noreply.github.com>
2021-03-20 05:08:52 +0900

    Update LeeHanSeong7.js

--- programmers/42628/javascript/LeeHanSeong7.js
+++ programmers/42628/javascript/LeeHanSeong7.js
@@ -1,113 +1,109 @@
-const operations = ["I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"];
+function solution(operations) {
+    class PriorityQueue{
+        Queue = [];
 
-class PriorityQueue{
-    Queue = [];
-    
-    swap =(a,b)=>{
-        const temp = this.Queue[a];
-        this.Queue[a] = this.Queue[b];
-        this.Queue[b] = temp;
-    }
-
-    BottomUp =(index)=>{
-        if (index == 0) return;
-        let parent = Math.floor((index-1)/2);
-        if (this.Queue[parent] < this.Queue[index]){
-            this.swap(parent, index);
-            this.BottomUp(parent);
+        swap =(a,b)=>{
+            const temp = this.Queue[a];
+            this.Queue[a] = this.Queue[b];
+            this.Queue[b] = temp;
         }
-    }
-    enQueue =(item)=>{
-        this.Queue.push(item);
-        if (this.Queue.length != 1)
-            this.BottomUp(this.Queue.length-1);
-    }
-    TopDown =(index)=>{
-        let left = index*2;
-        let right = index*2+1;
-        if (this.Queue.length <= left) return;
-        else if (this.Queue.length <= right){
-            if (this.Queue[left] > this.Queue[index]){
-                this.swap(left, index);
-                this.TopDown(left);
+
+        BottomUp =(index)=>{
+            if (index == 0) return;
+            let parent = Math.floor((index-1)/2);
+            if (this.Queue[parent] < this.Queue[index]){
+                this.swap(parent, index);
+                this.BottomUp(parent);
             }
         }
-        else{
-            const larger = (this.Queue[right]>this.Queue[left])?right:left;
-            if (this.Queue[larger] > this.Queue[index]){
-                this.swap(larger, index);
-                this.TopDown(larger);
-            }
+        enQueue =(item)=>{
+            this.Queue.push(item);
+            if (this.Queue.length != 1)
+                this.BottomUp(this.Queue.length-1);
         }
-    }
-    deQueueMax =()=>{
-        if (this.Queue.length == 0) return null;
-        if (this.Queue.length == 1) return this.Queue.pop();
-        
-        let res = this.Queue[0];
-        this.Queue[0] = this.Queue.pop();
-        
-        if (this.Queue.length != 1)
-            this.TopDown(0);
-        return res;
-    }
-    deQueueMin =()=>{
-        if (this.Queue.length == 0) return null;
-        if (this.Queue.length == 1) return this.Queue.pop();
-
-        let index = this.Queue.length-1;
-        let parent = Math.floor((index-1)/2);
-        let min = this.Queue[index];
-
-        for (let i=index; i>parent; i--){
-            if (min > this.Queue[i]){
-                min = this.Queue[i];
-                index = i;
-            }
-        }
-
-        if (index == this.Queue.length-1) return this.Queue.pop();
-
-        let res = min;
-        
-        this.Queue[index] = this.Queue.pop();
-        
-        if (this.Queue.length != 1)
-            this.BottomUp(index);
-
-        return res;
-    }
-}
-
-const PQ = new PriorityQueue();
-
-for(let i=0; i< operations.length; i++){
-    const opr = operations[i].split(' ');
-    switch(opr[0]){
-        case "I":
-            PQ.enQueue(parseInt(opr[1]));
-            console.log("I "+opr[1]);
-            break;
-        case "D":
-            if (opr[1] == 1){
-                //PQ.deQueueMax();
-                console.log("D 1 "+PQ.deQueueMax());
+        TopDown =(index)=>{
+            let left = index*2;
+            let right = index*2+1;
+            if (this.Queue.length <= left) return;
+            else if (this.Queue.length <= right){
+                if (this.Queue[left] > this.Queue[index]){
+                    this.swap(left, index);
+                    this.TopDown(left);
+                }
             }
             else{
-                //PQ.deQueueMin();
-                console.log("D -1"+PQ.deQueueMin());
+                const larger = (this.Queue[right]>this.Queue[left])?right:left;
+                if (this.Queue[larger] > this.Queue[index]){
+                    this.swap(larger, index);
+                    this.TopDown(larger);
+                }
             }
-            break;
-        default: break;
+        }
+        deQueueMax =()=>{
+            if (this.Queue.length == 0) return null;
+            if (this.Queue.length == 1) return this.Queue.pop();
+
+            let res = this.Queue[0];
+            this.Queue[0] = this.Queue.pop();
+
+            if (this.Queue.length != 1)
+                this.TopDown(0);
+            return res;
+        }
+        deQueueMin =()=>{
+            if (this.Queue.length == 0) return null;
+            if (this.Queue.length == 1) return this.Queue.pop();
+
+            let index = this.Queue.length-1;
+            let parent = Math.floor((index-1)/2);
+            let min = this.Queue[index];
+
+            for (let i=index; i>parent; i--){
+                if (min > this.Queue[i]){
+                    min = this.Queue[i];
+                    index = i;
+                }
+            }
+
+            if (index == this.Queue.length-1) return this.Queue.pop();
+
+            let res = min;
+
+            this.Queue[index] = this.Queue.pop();
+
+            if (this.Queue.length != 1)
+                this.BottomUp(index);
+
+            return res;
+        }
     }
-    console.log(PQ.Queue);
+
+    const PQ = new PriorityQueue();
+
+    for(let i=0; i< operations.length; i++){
+        const opr = operations[i].split(' ');
+        switch(opr[0]){
+            case "I":
+                PQ.enQueue(parseInt(opr[1]));
+                break;
+            case "D":
+                if (opr[1] == 1){
+                    PQ.deQueueMax();
+                }
+                else{
+                    PQ.deQueueMin();
+                }
+                break;
+            default: break;
+        }
+    }
+
+    let max = PQ.deQueueMax();
+    if (max == null) return [0,0];
+    else{
+        let min = PQ.deQueueMin();
+        if (min == null) min = max;
+
+        return [max,min];
+    }
 }
-
-// let max = PQ.deQueueMax();
-// if (max == null) return [0,0];
-// else{
-//     let min = PQ.deQueueMin();
-//     if (min == null) min = max;
-
-//     return [max,min];
-// }
