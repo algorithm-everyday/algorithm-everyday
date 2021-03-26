facf0bdf92ffa483421c48d9e75743894a0b594f
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-03-26 22:00:10 +0900

    [#77] Update gon125.swift

--- programmers/42746/swift/gon125.swift
+++ programmers/42746/swift/gon125.swift
@@ -1,5 +1,7 @@
 import Foundation
 /// - Complexity: O(nlogn)
 func solution(_ numbers:[Int]) -> String {
-    numbers.map { String($0) }.sorted(by: { (s1:String, s2:String) in s1 + s2 > s2 + s1 }).reduce("", +).replacingOccurrences(of: "^0+", with: "0", options: .regularExpression)
+    let sortedStrings = numbers.map { String($0) }.sorted(by: { $0+$1 > $1+$0 })
+    let stringSum = sortedStrings.reduce("", +)
+    return stringSum.first == "0" ? "0" : stringSum
 }
