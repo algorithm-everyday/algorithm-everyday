b729e7022aa4774f460635db46db2e4ded9040fa
Geonhyeong Lim <geonhyeong.lim@icloud.com>
2021-06-12 16:34:15 +0900

    Update gon125.swift

--- programmers/145/gon125.swift
+++ programmers/145/gon125.swift
@@ -6,20 +6,29 @@ func solution(_ s:String) -> Int {
     var result = 0
     // O(n)
     for x in 0..<s.count {
-        var stack = [Character]()
-        // O(n)
-        (s[x...] + s[0..<x]).forEach {
-            if let peek = stack.last, peek.matches($0) {
-                stack.popLast()!
-            } else {
-                stack.append($0)
-            }
+        if (s[x...] + s[0..<x]).isCorrect {
+            result += 1
         }
-        if stack.isEmpty { result += 1 }
     }
     
     return result
 }
+
+extension Sequence where Element == Parenthesis {
+    var isCorrect: Bool {
+        var stack = [Character]()
+        // O(n)
+        for c in self {
+            if c.isLeft { stack.append(c) }
+            else if let peek = stack.last, peek.matches(c) {
+                stack.popLast()!
+            } else { return false }
+        }
+        if stack.isEmpty { return true }
+        else { return false }
+    }
+}
+
 
 typealias Parenthesis = Character
 
@@ -29,6 +38,13 @@ extension Parenthesis {
         case "(": return p == ")"
         case "{": return p == "}"
         case "[": return p == "]"
+        default: return false
+        }
+    }
+    
+    var isLeft: Bool {
+        switch self {
+        case "(", "{", "[": return true
         default: return false
         }
     }
