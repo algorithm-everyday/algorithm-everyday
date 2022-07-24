09bf2ee7488b8dfa19b534e522476a28192eff48
vpdh98 <33365675+vpdh98@users.noreply.github.com>
2022-07-24 16:47:37 +0900

    77-vpdh98 (#154)
    * Create vpdh98.swift
    
    * Update vpdh98.swift

---
+++ programmers/42746/swift/vpdh98.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+extension Array where Element == Int{
+    var string:String{
+        var temp:String = ""
+        for i in self{
+            temp+=String(i)
+        }
+        return temp
+    }
+}
+
+func priority(_ n1:Int,_ n2:Int)->Bool{
+    var n1 = String(n1)
+    var n2 = String(n2)
+    return Int(n1+n2)! > Int(n2+n1)!
+}
+
+func solution(_ numbers:[Int]) -> String {
+    var n:Int = 1
+    var numbers:[Int] = numbers
+    numbers.sort(){priority($0,$1)}
+    
+    return numbers[0]==0 ? "0" : numbers.string
+}
